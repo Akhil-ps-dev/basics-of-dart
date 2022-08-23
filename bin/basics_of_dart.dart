2d4231d45c2fc80979e7445b940c0b1602eaed7a
Chatter-Boo <psakhil211@gmail.com>
2022-08-23 15:32:26 +0530

    parse

--- bin/basics_of_dart.dart
+++ bin/basics_of_dart.dart
@@ -12,10 +12,8 @@ void main() {
   var num1 = stdin.readLineSync();
   var num2 = stdin.readLineSync();
 
-  if (num1 == null) {
-    num1 = '0';
-  }
-  var sum1 = int.parse(num1);
+  
+  var sum1 = int.parse(num1!);
 var sum2 = int.parse(num2!);
 print( "the sum os ${sum1 + sum2}");
 
